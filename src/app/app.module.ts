8786078acccb3fac7c664bce82392b8fd73e3d32
lucper27 <lucasperuchena.27@gmail.com>
2022-06-26 14:31:32 +0200

    added: entities (Offer,Professional), home, layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {HttpRequestIntercept} from "./config/interceptor/http-request-interceptor.interceptor";
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { HomeComponent } from './home/home/home.component';
+import { OfferListComponent } from './entities/offer/offer-list/offer-list.component';
+import { ProfessionalDetailComponent } from './entities/professional/professional-detail/professional-detail.component';
+import { ProfessionalListComponent } from './entities/professional/professional-list/professional-list.component';
+import {OfferDetailComponent} from "./entities/offer/offer-detail/offer-detail.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomeComponent,
+    OfferDetailComponent,
+    OfferListComponent,
+    ProfessionalDetailComponent,
+    ProfessionalListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: HttpRequestIntercept,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/entities/professional/professional-detail/professional-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-professional-detail',
+  templateUrl: './professional-detail.component.html',
+  styleUrls: ['./professional-detail.component.scss']
+})
+export class ProfessionalDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
