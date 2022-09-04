41499ea81647fa10fba753d9a032a7c9970dddd2
lucper27 <lucasperuchena.27@gmail.com>
2022-09-04 09:55:14 +0200

    Commit before first push on Github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home/home.component";
+import {ProfessionalDetailComponent} from "./entities/professional/professional-detail/professional-detail.component";
 
 const routes: Routes = [
   { path: '',
     component: HomeComponent,
-    pathMatch: 'full' }
+    pathMatch: 'full'
+  },
+  {
+   path: 'profesionaldetail',
+   component: ProfessionalDetailComponent,
+   pathMatch: 'full'
+  }
 ];
 
 @NgModule({
